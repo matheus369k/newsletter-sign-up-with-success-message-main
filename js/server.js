2de7fb9dfae4541a4ed340ca880bc2e9b9efc578
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-12-03 15:48:31 -0300

    Project updateing: responsive creater and fuction finaly.....

--- js/server.js
+++ js/server.js
@@ -38,8 +38,86 @@ function Verification() {
 
     MsgError('valid');
 
+    const fother = document.querySelector('body');
+
+    $(fother.children).remove();
+
+    const div = document.createElement('div');
+
+    div.setAttribute('class', 'contianer_confirmation');
+
+    div.setAttribute('id', 'div_confirmation');
+
+    $(fother).append(div);
+
+    const listCreater = {
+        img: './assets/images/icon-success.svg',
+        h1: 'Thanks for subscribing!', 
+        p: 'A confirmation email has been sent to', 
+        span: ` ${inputValue}. `,
+        p_cont: 'Please open it and click the button inside to confirm your subscription.',
+        button: 'Dismiss message'
+    }
+    
+    const listCreaterKeys = Object.keys(listCreater)
+    const listCreaterValues = Object.values(listCreater)
+
+    for (let index = 0; index < 6; index++) {
+        
+        if ( index === 4) 
+        {
+            document.querySelector('.text').innerHTML+=`${listCreaterValues[index]}`;
+
+            continue
+
+        }
+
+        const creatertag = document.createElement(`${listCreaterKeys[index]}`)
+
+        if (index === 2) 
+        {
+            
+            creatertag.setAttribute('class', 'text')
+
+        }
+        
+        if (index === 0) 
+        {
+
+            creatertag.setAttribute('src', `${listCreaterValues[index]}`)
+
+        } 
+        else 
+        {
+            
+            $(creatertag).text(`${listCreaterValues[index]}`);
+        }
+
+        if ( index === 3 ) 
+        {
+        
+            $('.text').append(creatertag);
+
+            continue
+            
+        }
+        
+        $(div).append(creatertag);
+        
+    }
+
+    //const p = document.createElement('p')
+
+    //$(p).text(`A confirmation email has been sent to ${inputValue}. Please open it and click the button inside to
+    //confirm your subscription.`);
+
+    //const btn = document.createElement('button');
+
+    //$(btn).text('Dismiss message');
+    
+
     return
-}
+};
 
 
 function MsgError(verif) {
